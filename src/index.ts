c1a1e413e1cd9fab6e891f7ea392e145f71ac058
Gui <guil.oliveira98@gmail.com>
2020-08-31 21:27:23 -0300

    Add Twitter API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello world!");
+import Twitter from "twitter";
+require("dotenv").config();
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+listenTwitterForKeyword("up");
+
+function listenTwitterForKeyword(keyword: string) {
+  var stream = client.stream("statuses/filter", { track: keyword });
+  stream.on("data", function (event) {
+    if (event.limit) return;
+    console.log(event.text);
+  });
+
+  stream.on("error", function (error) {
+    throw error;
+  });
+}
